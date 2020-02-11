e4559181663f057d575a9c3fc3b8e18529fe6c2d
Zack <zotumlu78@gmail.com>
2020-02-11 18:16:42 -0500

    Updates

---
+++ precache-manifest.79c29438a0c4a96cb733e9c2a786548a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500c059d2ba35db4c3e8a4913edc48a7",
+    "url": "/Click/index.html"
+  },
+  {
+    "revision": "a3e20d0fb6d4ff5e186e",
+    "url": "/Click/static/css/main.4db81768.chunk.css"
+  },
+  {
+    "revision": "e6f9547efb5ee740c294",
+    "url": "/Click/static/js/2.79cf0ce7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Click/static/js/2.79cf0ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e20d0fb6d4ff5e186e",
+    "url": "/Click/static/js/main.0fcf3e55.chunk.js"
+  },
+  {
+    "revision": "7ae3e50883dc1e79f3e0",
+    "url": "/Click/static/js/runtime-main.93fd076a.js"
+  }
+]);
